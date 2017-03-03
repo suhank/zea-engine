e686b220de57a6a499c358a5e9c1c552fe348dc4
Phil Taylor <phil@visualive.io>
2017-03-03 09:49:24 -0500

    ixed bugs and improved quality of VLH encoding.

--- src/Renderer/Shaders/DecompHDRShader.js
+++ src/Renderer/Shaders/DecompHDRShader.js
@@ -32,6 +32,8 @@ varying vec2 v_texCoord;
 uniform sampler2D ldrSampler;
 uniform sampler2D cdmSampler;
 
+
+const float eps = 0.001;
 vec3 decodeHDR(const in vec3 ldrPixel, const in float cdmAlpha) {
     float avg = (cdmAlpha * 16.0 - 8.0);
     float scl = 1.0;
@@ -41,9 +43,10 @@ vec3 decodeHDR(const in vec3 ldrPixel, const in float cdmAlpha) {
     color.z = (tan((ldrPixel.z-0.5)*1.5)/scl)+avg;
 
     // convert from logarithmic curve to linear curve.
-    color.x = pow(10.0, color.x);
-    color.y = pow(10.0, color.y);
-    color.z = pow(10.0, color.z);
+    // subtract the epsilon that was added during encoding.
+    color.x = pow(10.0, color.x) - eps;
+    color.y = pow(10.0, color.y) - eps;
+    color.z = pow(10.0, color.z) - eps;
     return color;
 }
 
