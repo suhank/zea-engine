fc54c7aae6247a613afeb4ee5052a5bf0226712d
Philip Taylor <phil@hordesoftware.co>
2019-06-17 12:17:49 -0400

    Fixed up MultiChoiceParameter.

--- src/SceneTree/Parameters/MultiChoiceParameter.js
+++ src/SceneTree/Parameters/MultiChoiceParameter.js
@@ -1,11 +1,20 @@
 import {
   Parameter
 } from './Parameter.js';
+import {
+  NumberParameter
+} from './NumberParameter.js';
 
-class MultiChoiceParameter extends Parameter {
-  constructor(name, choices, index, type) {
-    super(name, choices[index], type);
+class MultiChoiceParameter extends NumberParameter {
+  constructor(name, index, choices) {
+    super(name, index, [0, choices.length], 1);
+    this.choices = choices;
   }
+
+  getChoices() {
+  	return this.choices;
+  }
+
 };
 
 export {
