5a04a590a0c3fd11488bd7f0cf1223049613cb34
Froilan Moreno <fonofondo@gmail.com>
2020-01-20 13:32:39 -0500

    feat: Add storybook with zea theme (#48)

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import zeaTheme from './zea-theme'
+
+addons.setConfig({
+  theme: zeaTheme,
+})
---
+++ .storybook/zea-theme.js
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming/create'
+
+const zeaTheme = create({
+  base: 'light',
+
+  colorPrimary: '#792ca7',
+  colorSecondary: '#8f49b8',
+
+  brandTitle: 'Zea Web Components',
+  brandUrl: 'https://zea.live',
+  brandImage: 'https://storage.googleapis.com/misc-assets/zea-logo.png',
+})
+
+export default zeaTheme
