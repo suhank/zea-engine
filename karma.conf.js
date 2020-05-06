981440f59dc0ea530cd2b74790716679d483c162
Philip Taylor <phil@zea.live>
2020-05-06 14:17:47 -0400

    very basic test passing.

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,26 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
-    files: ['tests/**/*.js'],
+    files: [
+      /* https://github.com/karma-runner/karma/issues/2955 */
+      { pattern: 'dist/index.esm.js', type: 'module', included: true },
+      { pattern: 'tests/**/*.js', type: 'module' }
+    ],
     reporters: ['progress'],
     port: 9876,  // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+
+    browsers: ['Chrome', 'ChromeHeadless', 'MyHeadlessChrome'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions',
+                '--no-first-run', '--disable-background-networking',
+                '--remote-debugging-port=9223']
+      }
+    },
+
     autoWatch: false,
     // singleRun: false, // Karma captures browsers, runs the tests and exits
     concurrency: Infinity
