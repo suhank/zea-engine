c805a19ab29d0bf0f6a33b3f8af059ae55605503
Philip Taylor <phil@zea.live>
2020-05-07 19:49:36 -0400

    writing out test image is now working.

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,20 @@
 import { Scene, GLRenderer, Vec3 } from '../dist/index.esm.js'
 
+
+const BASE64_MARKER = ';base64,';
+function dataURIToBlob(dataURI) {
+  const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  const base64 = dataURI.substring(base64Index);
+  const raw = window.atob(base64);
+  const rawLength = raw.length;
+  const array = new Uint8Array(rawLength);
+
+  for(let i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return new Blob([array], { type: 'image/png' }); 
+}
+
 describe('HelloWorld', () => {
   it('Render a simple grid', async () => {
     const div = document.createElement('div')
@@ -21,78 +36,27 @@ describe('HelloWorld', () => {
       .getCamera()
       .setPositionAndTarget(new Vec3(2, 2, 1.7), new Vec3(0, 0, 0.4))
 
-    // renderer.redrawOccured.connect(() => {
+    renderer.getViewport().draw()
+    console.log(renderer.__glcanvas)
 
-      renderer.getViewport().draw()
-      console.log('in callback')
-      console.log(renderer.__glcanvas)
-      // console.log(renderer.__gl)
+    const blob = dataURIToBlob(renderer.__glcanvas.toDataURL());
+    
+    const formData = new FormData()
+    formData.append('target-dir', '/Users/mauro/workspace/zea/engine/tests/')
+    formData.append('test-case-title', 'Zea hello world')
+    formData.append('file-to-test', blob, 'canvas.png')
+    console.log(formData)
 
-      const gl = renderer.__gl
-      var pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
-      gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
-      // console.log(pixels); // Uint8Array
-
-      // pixels.forEach(v => {
-      //   if (v != 128 && v != 255)
-      //     console.log(v)
-      // })
-
-      const outcanvas = document.createElement('canvas')
-      const w = gl.drawingBufferWidth
-      const h = gl.drawingBufferHeight
-      var ctx = outcanvas.getContext("2d");
-      // var UAC = new Uint8ClampedArray( pixels, w, h);
-      // var DAT = new ImageData(UAC, w, h);
-      // ctx.putImageData( DAT, 0, 0 );
-
-      var imageData = ctx.createImageData(w, h);
-      for (var i = 0; i < imageData.data.length; i++) {
-        imageData.data[i] = pixels[i];
-      }
-      ctx.putImageData(imageData, 0, 0);
-      const dataUrl = outcanvas.toDataURL();
-      console.log('dataUrl')
-      console.log(dataUrl)
-      
-      // var DAT = new ImageData(UAC, wid, hig);
-      // ctx.putImageData( pixels, ORG_X, ORG_Y );  
-      // const base64Data = btoa(String.fromCharCode.apply(null, pixels));
-      // console.log(base64Data);
-
-    // var c = renderer.__glcanvas.getContext('3d');
-    // var p = c.getImageData(10, 10, 1, 1).data; 
-    //   console.log(p)
-      // const dataUrl = renderer.__glcanvas.toDataURL()
-      // console.log('dataUrl')
-      // console.log(dataUrl)
-
-      renderer.__glcanvas.toBlob(function (err, blob) {
-        console.log(err)
-        console.log('blob', blob)
-        // {{{ Server.
-        const formData = new FormData()
-        formData.append(
-          'target-dir',
-          '/Users/mauro/workspace/zea/engine/tests/'
-        )
-        formData.append('test-case-title', 'Zea hello world')
-        formData.append('file-to-test', blob, 'canvas.png')
-        console.log(formData)
-
-        fetch('http://localhost:8080/files/test', {
-          body: formData,
-          method: 'post',
-        }).then((response) => {
-          response.json().then((json) => {
-            console.log(json)
-          })
-        })
-        // }}}
+    fetch('http://localhost:8080/files/test', {
+      body: formData,
+      method: 'post',
+    }).then((response) => {
+      response.json().then((json) => {
+        console.log(json)
+        assert.equal(1, 1)
       })
+    })
 
-      assert.equal(1, 1)
-    // })
 
     renderer.resumeDrawing()
   })
