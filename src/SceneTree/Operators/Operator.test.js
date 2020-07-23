aee42a7bb8732674092354be4344b92966e17242
mauroporras <mauroporrasc@gmail.com>
2020-07-23 14:24:08 -0500

    style: Prettier format

--- src/SceneTree/Operators/Operator.test.js
+++ src/SceneTree/Operators/Operator.test.js
@@ -13,7 +13,7 @@ class AddFloatsOperator extends Operator {
     this.addInput(new OperatorInput('B'))
     this.addOutput(new OperatorOutput('C'))
   }
-  
+
   evaluate() {
     const a = this.getInput('A').getValue()
     const b = this.getInput('B').getValue()
@@ -23,26 +23,25 @@ class AddFloatsOperator extends Operator {
 
 sgFactory.registerClass('AddFloatsOperator', AddFloatsOperator)
 
-
 // Note: this operatore modifies a value in the output attribute
 // By reading and then changing. This feature allows us to combine operators
 // to compute complex results. (See BoundingBox operators).
 class ScaleFloatOperator extends Operator {
   constructor(name) {
     super(name)
-    this.addInput(new OperatorInput("ScaleValue"));
-    this.addOutput(new OperatorOutput("Result", OperatorOutputMode.OP_READ_WRITE));
+    this.addInput(new OperatorInput('ScaleValue'))
+    this.addOutput(new OperatorOutput('Result', OperatorOutputMode.OP_READ_WRITE))
   }
 
   evaluate() {
     let scaleValue = 1.0
-    const inParam = this.getInput("ScaleValue").getParam()
+    const inParam = this.getInput('ScaleValue').getParam()
     if (inParam) {
       scaleValue = inParam.getValue()
     }
     // Read the value, modify and return.
-    const value = this.getOutput("Result").getValue()
-    this.getOutput("Result").setClean(value * scaleValue)
+    const value = this.getOutput('Result').getValue()
+    this.getOutput('Result').setClean(value * scaleValue)
   }
 }
 
@@ -56,9 +55,9 @@ describe('Operator', () => {
     const bParam = new NumberParameter('B')
     const myParam = new NumberParameter('MyParam')
 
-    addOperator.getInput("A").setParam(aParam)
-    addOperator.getInput("B").setParam(bParam)
-    addOperator.getOutput("C").setParam(myParam)
+    addOperator.getInput('A').setParam(aParam)
+    addOperator.getInput('B').setParam(bParam)
+    addOperator.getOutput('C').setParam(myParam)
 
     aParam.setValue(3)
     bParam.setValue(2.5)
@@ -74,8 +73,8 @@ describe('Operator', () => {
     const scaleParam = new NumberParameter('A', 2)
     const resultParam = new NumberParameter('MyParam', 3)
 
-    scaleOperator.getInput("ScaleValue").setParam(scaleParam)
-    scaleOperator.getOutput("Result").setParam(resultParam)
+    scaleOperator.getInput('ScaleValue').setParam(scaleParam)
+    scaleOperator.getOutput('Result').setParam(resultParam)
 
     expect(resultParam.isDirty()).toEqual(true)
     expect(resultParam.getValue()).toEqual(6)
@@ -94,15 +93,15 @@ describe('Operator', () => {
     const bParam = new NumberParameter('B', 3.5)
     const myParam = new NumberParameter('AddScaleFloatOperator-MyParam')
 
-    addOperator.getInput("A").setParam(aParam)
-    addOperator.getInput("B").setParam(bParam)
-    addOperator.getOutput("C").setParam(myParam)
+    addOperator.getInput('A').setParam(aParam)
+    addOperator.getInput('B').setParam(bParam)
+    addOperator.getOutput('C').setParam(myParam)
 
     const scaleOperator = new ScaleFloatOperator()
     const scaleParam = new NumberParameter('A', 2)
 
-    scaleOperator.getInput("ScaleValue").setParam(scaleParam)
-    scaleOperator.getOutput("Result").setParam(myParam)
+    scaleOperator.getInput('ScaleValue').setParam(scaleParam)
+    scaleOperator.getOutput('Result').setParam(myParam)
 
     expect(myParam.isDirty()).toEqual(true)
     expect(myParam.getValue()).toEqual(11)
@@ -114,7 +113,7 @@ describe('Operator', () => {
     expect(myParam.getValue()).toEqual(5.5)
     expect(myParam.isDirty()).toEqual(false)
   })
-  
+
   it('save to JSON (serialization).', () => {
     const addOperator = new AddFloatsOperator()
     const parameterOwner = new BaseItem('Foo')
@@ -122,17 +121,18 @@ describe('Operator', () => {
     const bParam = parameterOwner.addParameter(new NumberParameter('B'))
     const myParam = parameterOwner.addParameter(new NumberParameter('MyParam'))
 
-    addOperator.getInput("A").setParam(aParam)
-    addOperator.getInput("B").setParam(bParam)
-    addOperator.getOutput("C").setParam(myParam)
+    addOperator.getInput('A').setParam(aParam)
+    addOperator.getInput('B').setParam(bParam)
+    addOperator.getOutput('C').setParam(myParam)
 
     aParam.setValue(3)
     bParam.setValue(2.5)
 
-    const expOutput = '{\"name\":\"\",\"type\":\"AddFloatsOperator\",\"inputs\":[{\"paramPath\":[\"Foo\",\"A\"]},{\"paramPath\":[\"Foo\",\"B\"]}],\"outputs\":[{\"paramPath\":[\"Foo\",\"MyParam\"],\"paramBindIndex\":0}]}'
+    const expOutput =
+      '{"name":"","type":"AddFloatsOperator","inputs":[{"paramPath":["Foo","A"]},{"paramPath":["Foo","B"]}],"outputs":[{"paramPath":["Foo","MyParam"],"paramBindIndex":0}]}'
     expect(JSON.stringify(addOperator.toJSON())).toEqual(expOutput)
   })
-  
+
   it('load from JSON (serialization).', () => {
     const parameterOwner = new BaseItem('Foo')
     parameterOwner.addParameter(new NumberParameter('A'))
@@ -149,7 +149,7 @@ describe('Operator', () => {
     addOperator.fromJSON(input, {
       resolvePath: (path, cb) => {
         cb(parameterOwner.resolvePath(path))
-      }
+      },
     })
 
     parameterOwner.getParameter('A').setValue(3)
