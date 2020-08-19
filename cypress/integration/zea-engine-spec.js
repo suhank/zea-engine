bcca69a2888c0e2e4af57d48ec4d1d060bac4859
mauroporras <mauroporrasc@gmail.com>
2020-08-19 09:57:22 -0500

    test: Load an OBJ asset

--- cypress/integration/zea-engine-spec.js
+++ cypress/integration/zea-engine-spec.js
@@ -94,4 +94,28 @@ describe('Zea Engine', () => {
       cy.get('canvas').percySnapshot(`Labels ${variant}`)
     })
   })
+
+  it('Loads an OBJ asset', () => {
+    cy.visit('LoadAnOBJAsset.html', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage')
+      },
+    })
+
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-Loading')
+
+    cy.window().then((win) => {
+      const variant = 'front'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`OBJ asset ${variant}`)
+    })
+
+    cy.window().then((win) => {
+      const variant = 'back'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`OBJ asset ${variant}`)
+    })
+  })
 })
