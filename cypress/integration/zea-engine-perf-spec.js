7e42ccbbfb9d1b1e167ede31c0e97f9cda46a78f
mauroporras <mauroporrasc@gmail.com>
2020-09-02 21:47:35 -0500

    test(e2e): Fade instead of random colors

--- cypress/integration/zea-engine-perf-spec.js
+++ cypress/integration/zea-engine-perf-spec.js
@@ -1,11 +1,6 @@
 describe('Zea Engine-Performance', () => {
   it('stress test memory', () => {
-    cy.visit('memory-test.html', {
-      // onBeforeLoad(win) {
-      //   cy.spy(win, 'postMessage').as('postMessage')
-      // },
-    })
-    // cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-Loading')
+    cy.visit('memory-test.html', { timeout: 70000 })
     cy.get('canvas').percySnapshot(`memory-test`)
   })
 })
