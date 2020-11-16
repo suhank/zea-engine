ac9aa5ad3fbeaee5074cd0be8a6d5c691a3881be
Philip Taylor <phil@zea.live>
2020-11-16 15:26:22 -0500

    test: implemented tests for fixed-size labels.

--- cypress/integration/zea-engine-spec.js
+++ cypress/integration/zea-engine-spec.js
@@ -9,30 +9,6 @@ describe('Zea Engine', () => {
     cy.get('canvas').percySnapshot('Simple plane')
   })
 
-  it('Renders labels', () => {
-    cy.visit('testing-e2e/labels.html', {
-      onBeforeLoad(win) {
-        cy.spy(win, 'postMessage').as('postMessage')
-      },
-    })
-
-    cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-Loading')
-
-    cy.window().then((win) => {
-      const variant = 'front'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`Labels ${variant}`)
-    })
-
-    cy.window().then((win) => {
-      const variant = 'back'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`Labels ${variant}`)
-    })
-  })
-
   it('Loads an OBJ asset', () => {
     cy.visit('testing-e2e/load-obj-asset.html', {
       onBeforeLoad(win) {
