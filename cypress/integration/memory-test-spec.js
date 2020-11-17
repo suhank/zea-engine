7180d13368e578eba2b15f4efeb2e6109e6c7bf4
Mauro Porras <mauroporrasc@gmail.com>
2020-11-17 17:50:16 -0500

    test(e2e): fix memory spec (#288)

--- cypress/integration/memory-test-spec.js
+++ cypress/integration/memory-test-spec.js
@@ -2,7 +2,7 @@ describe('Zea Engine performance', () => {
   it('Behaves properly under memory stress', () => {
     cy.visit('testing-e2e/memory-test.html')
 
-    cy.get('#start').click()
+    cy.get('#run').click()
 
     cy.get('#memoryStatus').should('have.text', 'Good')
     cy.get('#timeStatus').should('have.text', 'Good')
