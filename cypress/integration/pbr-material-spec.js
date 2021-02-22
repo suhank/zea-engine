e66f8a2fe58074a780c92b1d670bcf52a2b73dc2
Philip Taylor <phil@zea.live>
2021-02-22 12:00:24 -0500

    test: textured-mesh run only a single promise.

--- cypress/integration/pbr-material-spec.js
+++ cypress/integration/pbr-material-spec.js
@@ -8,19 +8,5 @@ describe('pbr-material', () => {
 
     cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
     cy.get('canvas').percySnapshot('pbr-material')
-
-    cy.window().then((win) => {
-      const variant = 'variant-01'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`pbr-material - ${variant}`)
-    })
-
-    cy.window().then((win) => {
-      const variant = 'variant-02'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`pbr-material - ${variant}`)
-    })
   })
 })
