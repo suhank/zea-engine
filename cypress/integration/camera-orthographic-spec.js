1709f861c8355b0a3dc841a3fb468b97a38dbc1a
Philip Taylor <phil@zea.live>
2021-04-23 12:55:22 -0400

    feat: Camera now supports perspective and orthographic projections, and is able to interpolate between.

---
+++ cypress/integration/camera-orthographic-spec.js
@@ -0,0 +1,26 @@
+describe('camera-orthographic', () => {
+  it('Capture snapshots', () => {
+    cy.visit('testing-e2e/camera-orthographic.html', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage')
+      },
+    })
+
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
+    cy.get('canvas').percySnapshot('camera-orthographic')
+
+    cy.window().then((win) => {
+      const variant = 'variant-01'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`camera-orthographic - ${variant}`)
+    })
+
+    cy.window().then((win) => {
+      const variant = 'variant-02'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`camera-orthographic - ${variant}`)
+    })
+  })
+})
