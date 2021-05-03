502d9ff878f637d3482e77a8246f790d0b584447
Philip Taylor <phil@zea.live>
2021-05-03 12:13:04 -0400

    docs: cleaned up docs for view framing API.

--- src/Renderer/GLViewport.js
+++ src/Renderer/GLViewport.js
@@ -166,14 +166,17 @@ class GLViewport extends GLBaseViewport {
   }
 
   /**
-   * The frameView method.
-   * @param {array} treeItems - The treeItems value.
+   * Calculates a new camera position that frames all the items passed in `treeItems` array, moving
+   * the camera to a point where we can see all of them.
+   * > See Camera.frameView
+   * @param {array} treeItems - The array of TreeItem.
    */
   frameView(treeItems) {
     if (this.__width > 0 && this.__height > 0) {
       this.__camera.frameView(this, treeItems)
     } else {
-      // Wait till the window is resized and try again.
+      // Sometimes thew renderer is not yet setup, so here we
+      // wait till the window is resized and try again.
       this.once('resized', () => this.frameView())
     }
   }
