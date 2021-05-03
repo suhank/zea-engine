f38b026ed9c58dc2d5ac2c142829892809792cb2
Philip Taylor <phil@zea.live>
2021-05-03 16:57:05 -0400

    refactor: GLLinesPass now exclusively manages renderingg lines geometries. This is so Silhouettes can be draw before lines, to avoid Silhouettes around lines geometries.

---
+++ cypress/integration/circle-spec.js
@@ -0,0 +1,26 @@
+describe('circle', () => {
+  it('Capture snapshots', () => {
+    cy.visit('testing-e2e/circle.html', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage')
+      },
+    })
+
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
+    cy.get('canvas').percySnapshot('circle')
+
+    cy.window().then((win) => {
+      const variant = 'variant-01'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`circle - ${variant}`)
+    })
+
+    cy.window().then((win) => {
+      const variant = 'variant-02'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`circle - ${variant}`)
+    })
+  })
+})
