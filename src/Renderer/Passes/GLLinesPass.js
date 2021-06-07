bbe1338918590d9d0b46da68e37b0443a7fddca5
Philip Taylor <phil@zea.live>
2021-06-07 16:38:57 -0400

    WIP: Fattenling lines using a post processing shader/

--- src/Renderer/Passes/GLLinesPass.js
+++ src/Renderer/Passes/GLLinesPass.js
@@ -16,6 +16,25 @@ class GLLinesPass extends GLOpaqueGeomsPass {
   }
 
   /**
+   * The init method.
+   * @param {GLBaseRenderer} renderer - The renderer value.
+   * @param {number} passIndex - The index of the pass in the GLBAseRenderer
+   */
+  init(renderer, passIndex) {
+    super.init(renderer, passIndex)
+
+    this.__geomDataBuffer = new GLTexture2D(this.__gl, {
+      type: renderer.__floatGeomBuffer ? 'FLOAT' : 'UNSIGNED_BYTE',
+      format: 'RGBA',
+      filter: 'NEAREST',
+      width: 1,
+      height: 2,
+    })
+    this.__geomDataBufferFbo = new GLFbo(gl, this.__geomDataBuffer, true)
+    this.__geomDataBufferFbo.setClearColor([0, 0, 0, 0])
+    this.fattenLinesShader = new FattenLinesShader(gl)
+  }
+  /**
    * The filterGeomItem method.
    * @param {GeomItem} geomItem - The geomItem value.
    * @return {boolean} - The return value.
@@ -44,6 +63,49 @@ class GLLinesPass extends GLOpaqueGeomsPass {
     this.__traverseTreeAndDraw(renderstate)
 
     gl.disable(gl.BLEND)
+  }
+  /**
+   * The drawGeomData method.
+   * @param {object} renderstate - The object tracking the current state of the renderer
+   */
+  drawGeomData(renderstate) {
+    if (this.__geomDataBufferFbo) {
+      const { region } = renderstate
+      if (this.__geomDataBuffer.width != region[2] || this.__geomDataBuffer.height != region[3]) {
+        this.__geomDataBuffer.resize(region[2], region[3])
+
+        this.fbo = gl.createFramebuffer()
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo)
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.TEXTURE_2D, this.depthTexture.glTex, 0)
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+      }
+      this.__geomDataBufferFbo.bindForWriting(renderstate)
+    }
+    super.drawGeomData(renderstate)
+
+    if (this.__geomDataBufferFbo) {
+      this.__geomDataBufferFbo.unbindForWriting(renderstate)
+
+      this.fattenLinesShader.bind(renderstate)
+
+      const unifs = renderstate.unifs
+
+      this.depthTexture.bindToUniform(renderstate, unifs.depthTexture)
+
+      if (!gl.renderbufferStorageMultisample) {
+        this.offscreenBuffer.bindToUniform(renderstate, unifs.colorTexture)
+      }
+
+      gl.uniform2f(unifs.screenSize.location, this.__width, this.__height)
+      gl.uniform1f(unifs.outlineThickness.location, this.renderer.outlineThickness)
+      gl.uniform4f(unifs.outlineColor.location, ...this.renderer.outlineColor.asArray())
+      gl.uniform1f(unifs.outlineDepthMultiplier.location, renderstate.outlineDepthMultiplier)
+      gl.uniform1f(unifs.outlineDepthBias.location, this.renderer.outlineDepthBias)
+
+      gl.uniform2f(unifs.depthRange.location, renderstate.depthRange[0], renderstate.depthRange[1])
+
+      this.quad.bindAndDraw(renderstate)
+    }
   }
 }
 
