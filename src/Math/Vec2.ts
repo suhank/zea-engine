cedc12070f3db5b2cd0365f2e2215a9be362cb79
Seiji-zea Smith <seiji.smith@zea.live>
2021-06-25 08:41:06 -0700

    Revert "update gitignore, tsconfig, added types"
    This reverts commit 0ae26c794e1d014bf8a6043e4b8d974592e28fa1.

--- src/Math/Vec2.ts
+++ src/Math/Vec2.ts
@@ -1,6 +1,5 @@
-//import { AttrValue } from './AttrValue.js'
+import { AttrValue } from './AttrValue.js'
 import { Registry } from '../Registry'
-import { BinReader } from '../SceneTree/BinReader'
 
 /**
  * Representing a Vec2(two-dimensional floating point vector). A Vec2 is for representing 2 dimensional values, such as screen coordinates or pixel coordinates within an image.
@@ -10,7 +9,7 @@ import { BinReader } from '../SceneTree/BinReader'
  *
  * @extends AttrValue
  */
-class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
+class Vec2 extends AttrValue {
   /**
    * Creates a Vec2.
    *
@@ -62,10 +61,9 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Number|Float32Array|Uint32Array|json} x - The x value. Default is 0.
    * @param {Number} y - The y value. Default is 0.
    */
-  __data: TypedArray | Record<string, any>;
+  constructor(x = 0, y = 0) {
+    super()
 
-  constructor(x: number | Float32Array | Uint32Array | Record<string, any> = 0, y = 0) {
-    
     if (x instanceof Float32Array || x instanceof Uint32Array || x instanceof Int32Array) {
       this.__data = x
     } else if (x instanceof ArrayBuffer) {
@@ -87,7 +85,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * Getter for `x` component.
    * @return {number} - Returns the x component.
    */
-  get x(): number {
+  get x() {
     return this.__data[0]
   }
 
@@ -95,7 +93,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * Setter for `x` component.
    * @param {number} val - The val param.
    */
-  set x(val: number) {
+  set x(val) {
     this.__data[0] = val
   }
 
@@ -103,7 +101,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * Getter for `y` component.
    * @return {number} - Returns the y component.
    */
-  get y(): number {
+  get y() {
     return this.__data[1]
   }
 
@@ -111,7 +109,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * Setter for `y` component.
    * @param {number} val - The val param.
    */
-  set y(val: number) {
+  set y(val) {
     this.__data[1] = val
   }
 
@@ -120,7 +118,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} x - The x component.
    * @param {number} y  - The y component.
    */
-  set(x: number, y: number): void {
+  set(x, y) {
     this.__data[0] = x
     this.__data[1] = y
   }
@@ -130,7 +128,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {Vec2} other - The other Vec2 to set from.
    */
-  setFromOther(other: Vec2): void {
+  setFromOther(other) {
     this.x = other.x
     this.y = other.y
   }
@@ -143,7 +141,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {boolean} - Returns `true` if are the same Vector, otherwise, `false`.
    */
-  equal(other: Vec2): boolean {
+  equal(other) {
     console.warn('Deprecated. Use #isEqual instead.')
     return this.isEqual(other)
   }
@@ -154,7 +152,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {boolean} - Returns `true` if are the same Vector, otherwise, `false`.
    */
-  isEqual(other: Vec2): boolean {
+  isEqual(other) {
     return this.x == other.x && this.y == other.y
   }
 
@@ -165,7 +163,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {boolean} - Returns `true` if the Vec2s are different, otherwise, `false`.
    */
-  notEquals(other: Vec2): boolean {
+  notEquals(other) {
     console.warn('Deprecated. Use #notEqual instead.')
     return this.notEqual(other)
   }
@@ -176,7 +174,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {boolean} - Returns `true` if the Vec2s are different, otherwise, `false`.
    */
-  notEqual(other: Vec2): boolean {
+  notEqual(other) {
     return this.x != other.x && this.y != other.y
   }
 
@@ -187,7 +185,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} precision - The precision to which the values must match.
    * @return {boolean} - Returns true or false.
    */
-  approxEqual(other: Vec2, precision: number = Number.EPSILON): boolean {
+  approxEqual(other, precision = Number.EPSILON) {
     return Math.abs(this.x - other.x) < precision && Math.abs(this.y - other.y) < precision
   }
 
@@ -197,7 +195,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to add.
    * @return {Vec2} - Returns a new Vec2.
    */
-  add(other: Vec2): Vec2 {
+  add(other) {
     return new Vec2(this.x + other.x, this.y + other.y)
   }
 
@@ -206,7 +204,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {Vec2} other - The other Vec2 to add.
    */
-  addInPlace(other: Vec2): void {
+  addInPlace(other) {
     this.x += other.x
     this.y += other.y
   }
@@ -217,7 +215,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to subtract.
    * @return {Vec2} - Returns a new Vec2.
    */
-  subtract(other: Vec2): Vec2 {
+  subtract(other) {
     return new Vec2(this.x - other.x, this.y - other.y)
   }
 
@@ -227,7 +225,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to subtract.
    * @return {Vec2} - Returns a new Vec2.
    */
-  subtractInPlace(other: Vec2): Vec2 {
+  subtractInPlace(other) {
     this.x -= other.x
     this.y -= other.y
     return this
@@ -239,7 +237,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} scalar - The scalar value.
    * @return {Vec2} - Returns a new Vec2.
    */
-  scale(scalar: number): Vec2 {
+  scale(scalar) {
     return new Vec2(this.x * scalar, this.y * scalar)
   }
 
@@ -248,7 +246,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {number} scalar - The scalar value.
    */
-  scaleInPlace(scalar: number): void {
+  scaleInPlace(scalar) {
     this.x *= scalar
     this.y *= scalar
   }
@@ -258,7 +256,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {Vec2} - Returns a new Vec2.
    */
-  invert(): Vec2 {
+  invert() {
     return new Vec2(1.0 / this.x, 1.0 / this.y)
   }
 
@@ -267,7 +265,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {Vec2} - The return value.
    */
-  invertInPlace(): Vec2 {
+  invertInPlace() {
     this.x = 1.0 / this.x
     this.y = 1.0 / this.y
     return this
@@ -279,7 +277,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to multiply with.
    * @return {Vec2} - Returns a new Vec2.
    */
-  multiply(other: Vec2): Vec2 {
+  multiply(other) {
     return new Vec2(this.x * other.x, this.y * other.y)
   }
 
@@ -288,7 +286,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {Vec2} other - The other Vec2 to multiply with.
    */
-  multiplyInPlace(other: Vec2): void {
+  multiplyInPlace(other) {
     this.x *= other.x
     this.y *= other.y
   }
@@ -298,7 +296,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {number} - Returns the length squared.
    */
-  lengthSquared(): number {
+  lengthSquared() {
     const x = this.__data[0]
     const y = this.__data[1]
     return x * x + y * y
@@ -309,7 +307,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {number} - Returns the length.
    */
-  length(): number {
+  length() {
     return Math.sqrt(this.lengthSquared())
   }
 
@@ -319,7 +317,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other value.
    * @return {number} - Returns the distance between vectors.
    */
-  distanceTo(other: Vec2): number {
+  distanceTo(other) {
     const x = this.__data[0] - other.x
     const y = this.__data[1] - other.y
     return Math.sqrt(x * x + y * y)
@@ -331,7 +329,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {Vec2} - Returns the Vec2 normalized.
    */
-  normalize(): Vec2 {
+  normalize() {
     const x = this.__data[0]
     const y = this.__data[1]
     let len = x * x + y * y
@@ -347,7 +345,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
   /**
    * Normalizes this Vec2 multiplying coordinate values by the inverse of the vector length.
    */
-  normalizeInPlace(): void {
+  normalizeInPlace() {
     const x = this.__data[0]
     const y = this.__data[1]
     let len = x * x + y * y
@@ -364,7 +362,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {number} - Returns the dot product.
    */
-  dot(other: Vec2): number {
+  dot(other) {
     return this.x * other.x + this.y * other.y
   }
 
@@ -374,7 +372,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {number} - Returns the cross product.
    */
-  cross(other: Vec2): number {
+  cross(other) {
     // just calculate the z-component
     return this.x * other.y - this.y * other.x
   }
@@ -385,7 +383,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {number} - Returns the angle in radians.
    */
-  angleTo(other: Vec2): number {
+  angleTo(other) {
     const cosine = this.normalize().dot(other.normalize())
     if (cosine > 1.0) return 0.0
     else if (cosine < -1.0) return Math.PI
@@ -398,7 +396,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {Vec2} other - The other Vec2 to compare with.
    * @return {number} - Returns the angle in radians.
    */
-  signedAngleTo(other: Vec2): number {
+  signedAngleTo(other) {
     const angle = this.angleTo(other)
     if (this.cross(other) < 0.0) return -angle
     else return angle
@@ -410,7 +408,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} angle - The angle of rotation.
    * @return {Vec2} - Returns the rotated vector.
    */
-  rotate(angle: number): Vec2 {
+  rotate(angle) {
     const cosA = Math.cos(angle)
     const sinA = Math.sin(angle)
     return new Vec2(this.x * cosA - this.y * sinA, this.x * sinA + this.y * cosA)
@@ -423,7 +421,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} t - Interpolation amount between the two inputs.
    * @return {Vec2} - Returns a new Vec2.
    */
-  lerp(other: Vec2, t: number): Vec2 {
+  lerp(other, t) {
     const ax = this.x
     const ay = this.y
     return new Vec2(ax + t * (other.x - ax), ay + t * (other.y - ay))
@@ -435,10 +433,10 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} scale - Length of the resulting vector. If omitted, a unit vector will be returned.
    * @return {Vec2} - The return value.
    */
-  setRandomDir(scale = 1.0): Vec2 {
+  setRandomDir(scale = 1.0) {
     const r = Math.random() * 2.0 * Math.PI
-    this.__data[0] = Math.cos(r) * scale
-    this.__data[1] = Math.sin(r) * scale
+    this.__data[0] = Math.cos(r) * zScale
+    this.__data[1] = Math.sin(r) * zScale
     return this
   }
 
@@ -448,7 +446,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} scale - The scale value.
    * @return {Vec2} - The return value.
    */
-  setRandom(scale = 1.0): Vec2 {
+  setRandom(scale = 1.0) {
     this.__data[0] = Math.random() * scale
     this.__data[1] = Math.random() * scale
     return this
@@ -459,7 +457,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @return {Vec2} - Returns a new Vec2.
    */
-  clone(): Vec2 {
+  clone() {
     return new Vec2(this.__data[0], this.__data[1])
   }
 
@@ -483,8 +481,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @return {Vec2} - Returns a new Vec2.
    * @private
    */
-
-  static create(...args: any[]): Vec2 {
+  static create(...args) {
     return new Vec2(...args)
   }
 
@@ -496,7 +493,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @deprecated
    * @private
    */
-  static createFromFloat32Buffer(buffer: ArrayBuffer, offset = 0): Vec2 {
+  static createFromFloat32Buffer(buffer, offset = 0) {
     console.warn('Deprecated, use #createFromBuffer instead')
     return this.createFromBuffer(buffer, offset * 4)
   }
@@ -509,7 +506,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @param {number} byteOffset - The offset value.
    * @return {Vec2} - Returns a new Vec2.
    */
-  static createFromBuffer(buffer: ArrayBuffer, byteOffset: number): Vec2 {
+  static createFromBuffer(buffer, byteOffset) {
     return new Vec2(new Float32Array(buffer, byteOffset, 2)) // 4 bytes per 32bit float
   }
 
@@ -519,7 +516,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @return {Vec2} - Returns a new Vec2.
    * @private
    */
-  static createFromFloat32Array(array: Float32Array): Vec2 {
+  static createFromFloat32Array(array) {
     return new Vec2(array)
   }
 
@@ -528,7 +525,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    * @return {number} - The return value.
    * @private
    */
-  static numElements(): number {
+  static numElements() {
     return 2
   }
 
@@ -538,9 +535,9 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
   /**
    * Encodes Vec2 Class as a JSON object for persistence.
    *
-   * @return {Record<string, number>} - The json object.
+   * @return {object} - The json object.
    */
-  toJSON(): Record<string, number> {
+  toJSON() {
     return {
       x: this.x,
       y: this.y,
@@ -552,7 +549,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {object} j - The json object.
    */
-  fromJSON(j: Record<string, number>): void {
+  fromJSON(j) {
     this.x = j.x
     this.y = j.y
   }
@@ -562,7 +559,7 @@ class Vec2 { // TODO: ok to drop AttrValue inheritenc? extends AttrValue
    *
    * @param {BinReader} reader - The reader value.
    */
-  readBinary(reader: BinReader): void {
+  readBinary(reader) {
     this.x = reader.loadFloat32()
     this.y = reader.loadFloat32()
   }
