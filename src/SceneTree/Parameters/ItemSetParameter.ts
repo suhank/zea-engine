7dc648937355856f26f04725f628efa75d2fb492
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-02 16:40:06 -0700

    updated tsconfig for itemSetParameter.ts
    - allows iteration of iterators

--- src/SceneTree/Parameters/ItemSetParameter.ts
+++ src/SceneTree/Parameters/ItemSetParameter.ts
@@ -1,59 +1,67 @@
-import { Parameter } from './Parameter-temp.js'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Parameter } from './Parameter'
+import { BaseItem } from '../../SceneTree/BaseItem'
 
 /** Class representing an item set parameter.
  * @extends Parameter
  * @private
  */
-class ItemSetParameter extends Parameter {
+class ItemSetParameter extends Parameter<Set<BaseItem>> {
+  protected filterFn: (...args: any) => unknown
+  protected items: Set<BaseItem>
   /**
    * Create an item set parameter.
    * @param {string} name - The name of the item set parameter.
-   * @param {any} filterFn - The filterFn value.
+   * @param {(...args: any[]) => unknown} filterFn - The filterFn value.
    */
-  constructor(name, filterFn) {
-    super(name, undefined, 'BaseItem')
-    this.__items = new Set()
-    this.__filterFn = filterFn // Note: the filter Fn indicates that users will edit the set.
+  constructor(name: string, filterFn: (...args: any[]) => unknown) {
+    super(name, new Set(), 'BaseItem')
+    // this.items = new Set() TODO:(refactor) necessary?
+    this.filterFn = filterFn // Note: the filter Fn indicates that users will edit the set.
   }
 
   /**
    * The setFilterFn method.
-   * @param {any} filterFn - The filterFn value.
+   * @param {(...args: any) => unknown} filterFn - The filterFn value.
    */
-  setFilterFn(filterFn) {
-    this.__filterFn = filterFn
+  setFilterFn(filterFn: (...args: any) => unknown): void {
+    this.filterFn = filterFn
   }
 
   /**
    * The getFilterFn method.
-   * @return {any} - The return value.
+   * @return {(...args: any) => unknown} - The return value.
    */
-  getFilterFn() {
-    return this.__filterFn
+  getFilterFn(): (...args: any) => unknown {
+    return this.filterFn
   }
 
   /**
    * The getItem method.
    * @param {number} index - The index param.
-   * @return {any} - The return value.
+   * @return {BaseItem} - The return value.
    */
-  getItem(index) {
-    return Array.from(this.__items)[index]
+  getItem(index: number): BaseItem | undefined {
+    if (!this.value) return undefined
+    return Array.from(this.value)[index]
   }
 
   /**
    * The addItem method.
-   * @param {any} item - The item value.
+   * @param {BaseItem} item - The item value.
    * @param {boolean} emitValueChanged - The emit value.
    * @return {boolean} - The return value.
    */
-  addItem(item, emitValueChanged = true) {
-    if (this.__filterFn && !this.__filterFn(item)) {
+  addItem(item: BaseItem, emitValueChanged = true): number | void {
+    if (this.filterFn && !this.filterFn(item)) {
       console.warn('ItemSet __filterFn rejecting item:', item.getPath())
-      return false
+      return
     }
-    this.__items.add(item)
-    const index = Array.from(this.__items).indexOf(item)
+    if (!this.value) this.value = new Set()
+
+    this.value.add(item)
+    const index = Array.from(this.value).indexOf(item)
     this.emit('itemAdded', { item, index })
     if (emitValueChanged) this.emit('valueChanged', {})
     return index
@@ -62,24 +70,26 @@ class ItemSetParameter extends Parameter {
   /**
    * Adds items to the parameter value
    *
-   * @param {Set} items - list of items to add to the parameter
+   * @param {Set<BaseItem>} items - list of items to add to the parameter
    * @param {boolean} [emitValueChanged=true]
    * @memberof ItemSetParameter
    */
-  addItems(items, emitValueChanged = true) {
-    items.forEach((item) => this.addItem(item, false))
+  addItems(items: Set<BaseItem>, emitValueChanged = true): void {
+    items.forEach((item: BaseItem) => this.addItem(item, false))
     if (emitValueChanged) this.emit('valueChanged', {})
   }
 
   /**
    * The removeItem method.
-   * @param {any} index - The index value.
+   * @param {number} index - The index value.
    * @param {boolean} emitValueChanged - The emit param.
-   * @return {any} - The return value.
+   * @return {BaseItem} - The return value.
    */
-  removeItem(index, emitValueChanged = true) {
-    const item = Array.from(this.__items)[index]
-    this.__items.delete(item)
+  removeItem(index: number, emitValueChanged = true): BaseItem | void {
+    if (!this.value) return
+
+    const item = Array.from(this.value)[index]
+    this.value.delete(item)
     this.emit('itemRemoved', { item, index })
     if (emitValueChanged) this.emit('valueChanged', {})
     return item
@@ -87,47 +97,52 @@ class ItemSetParameter extends Parameter {
 
   /**
    * The setItems method.
-   * @param {any} items - The item param.
+   * @param {Set<BaseItem>} items - The item param.
    * @param {boolean} emit - The emit param.
    */
-  setItems(items, emit = true) {
-    for (let i = this.__items.length - 1; i >= 0; i--) {
-      const item = this.__items[i]
-      if (!items.has(item)) {
-        this.removeItem(item, false)
+  setItems(items: Set<BaseItem>, emit = true): void {
+    if (!this.value) this.value = items
+    else {
+      for (let i = this.value.size - 1; i >= 0; i--) {
+        const item = Array.from(this.value)[i]
+        if (!items.has(item)) {
+          this.removeItem(i, false)
+        }
       }
-    }
-    for (const item of items) {
-      if (!this.__items.has(item)) {
-        this.addItem(item, false)
+
+      for (const item of items) {
+        if (!this.value.has(item)) {
+          this.addItem(item, false)
+        }
       }
+      if (emit) this.emit('valueChanged', {})
     }
-    if (emit) this.emit('valueChanged', {})
   }
 
   /**
    * The clearItems method.
-   * @param {boolean} emitValueChanged - The emit value.
+   * @param {boolean} emit - The emit value.
    */
-  clearItems(emitValueChanged = true) {
-    this.__items.clear()
+  clearItems(emitValueChanged = true): void {
+    if (!this.value) this.value = new Set()
+    this.value.clear()
     if (emitValueChanged) this.emit('valueChanged', {})
   }
 
   /**
    * The getNumItems method.
-   * @return {any} - The return value.
+   * @return {number} - The return value.
    */
-  getNumItems() {
-    return Array.from(this.__items).length
+  getNumItems(): number {
+    if (!this.value) this.value = new Set()
+    return Array.from(this.value).length
   }
-
   /**
    * The getValue method.
    * @return {any} - The return value.
    */
   getValue() {
-    return this.__items
+    return this.items
   }
 
   // ////////////////////////////////////////
@@ -135,19 +150,35 @@ class ItemSetParameter extends Parameter {
 
   /**
    * The toJSON method encodes this type as a json object for persistence.
-   * @param {object} context - The context value.
-   * @return {object} - The return value.
+   * @param {Record<string, any>} context - The context value.
+   * @return {Record<string, any>} - The return value.
    */
-  toJSON(context) {
-    return {}
+  toJSON(context?: Record<string, any>): Record<string, any> {
+    if (!this.value) this.value = new Set()
+
+    const items = []
+    for (const item of this.value) {
+      items.push(item.toJSON())
+    }
+
+    return {
+      value: items,
+    }
   }
 
   /**
    * The fromJSON method decodes a json object for this type.
-   * @param {object} j - The json object this item must decode.
-   * @param {object} context - The context value.
+   * @param {Record<string, any>} j - The json object this item must decode.
+   * @param {Record<string, any>} context - The context value.
    */
-  fromJSON(j, context) {}
+  fromJSON(j: Record<string, any>, context?: Record<string, any>): void {
+    if (!this.value) this.value = new Set()
+    for (const item of j.values) {
+      const baseItem = new BaseItem()
+      baseItem.fromJSON(item)
+      this.value.add(baseItem)
+    }
+  }
 
   // ////////////////////////////////////////
   // Clone
@@ -158,8 +189,9 @@ class ItemSetParameter extends Parameter {
    *
    * @return {ItemSetParameter} - Returns a new item set parameter.
    */
-  clone() {
-    const clonedParam = new ItemSetParameter(this.__name, this.__filterFn)
+  clone(): ItemSetParameter {
+    const clonedParam = new ItemSetParameter(this.name, this.filterFn)
+    if (this.value) clonedParam.setItems(this.value)
     return clonedParam
   }
 }
