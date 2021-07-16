21d61f94ce33804b0f4b048a8beb19d92c092ec4
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-16 15:00:15 -0700

    refactor: rename commits

--- src/Math/ts-test/ts-fn.test.js
+++
@@ -1,5 +0,0 @@
-import { sum } from './ts-fn'
-
-test('.js test file', () => {
-  expect(sum(1, 2)).toBe(3)
-})
--- src/Math/ts-test/ts-fn.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from './ts-fn';
-
-test('typescript test', () => {
-  expect(sum()).toBe(0);
-});
-
-test('typescript test 2', () => {
-  expect(sum(1, 2)).toBe(3);
-});
