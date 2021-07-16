1f8604de494cdcae078e2c19677561381991763a
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-16 14:55:36 -0700

    refactor: index-plugin migrated

--- src/index-plugins.ts
+++ src/index-plugins.ts
@@ -1,16 +1,14 @@
-import { ArchiveUnpackerPlugin } from './SceneTree/ResourceLoader/ArchiveUnpackerPlugin.js'
-import { JsonLoaderPlugin } from './SceneTree/ResourceLoader/JsonLoaderPlugin.js'
-import { TextLoaderPlugin } from './SceneTree/ResourceLoader/TextLoaderPlugin.js'
-import { BinaryLoaderPlugin } from './SceneTree/ResourceLoader/BinaryLoaderPlugin.js'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ArchiveUnpackerPlugin } from './SceneTree/ResourceLoader/ArchiveUnpackerPlugin'
+import { JsonLoaderPlugin } from './SceneTree/ResourceLoader/JsonLoaderPlugin'
+import { TextLoaderPlugin } from './SceneTree/ResourceLoader/TextLoaderPlugin'
+import { BinaryLoaderPlugin } from './SceneTree/ResourceLoader/BinaryLoaderPlugin'
 
 const archiveUnpackerPlugin = new ArchiveUnpackerPlugin()
-window.zeaEngine.resourceLoader.registerPlugin(archiveUnpackerPlugin)
-
+;(window as any).zeaEngine.resourceLoader.registerPlugin(archiveUnpackerPlugin)
 const jsonLoaderPlugin = new JsonLoaderPlugin()
-window.zeaEngine.resourceLoader.registerPlugin(jsonLoaderPlugin)
-
+;(window as any).zeaEngine.resourceLoader.registerPlugin(jsonLoaderPlugin)
 const textLoaderPlugin = new TextLoaderPlugin()
-window.zeaEngine.resourceLoader.registerPlugin(textLoaderPlugin)
-
+;(window as any).zeaEngine.resourceLoader.registerPlugin(textLoaderPlugin)
 const binaryLoaderPlugin = new BinaryLoaderPlugin()
-window.zeaEngine.resourceLoader.registerPlugin(binaryLoaderPlugin)
+;(window as any).zeaEngine.resourceLoader.registerPlugin(binaryLoaderPlugin)
