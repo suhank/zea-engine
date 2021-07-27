ba1a9acfe6b69d81172c9cef89149d82fd11f586
Philip Taylor <phil@zea.live>
2021-07-27 12:11:23 -0400

    test: migrated the 'transparent-geoms-sorting' test to use buttons instead of 'events', making it easier to run the test manually.

--- cypress/integration/transparent-geoms-sorting-spec.js
+++ cypress/integration/transparent-geoms-sorting-spec.js
@@ -9,29 +9,20 @@ describe('transparent-geoms-sorting', () => {
     cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
     cy.get('canvas').percySnapshot('transparent-geoms-sorting')
 
-    cy.window().then((win) => {
-      const variant = 'variant-01'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting - ${variant}`)
-    })
-    cy.window().then((win) => {
-      const variant = 'variant-02'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting - ${variant}`)
-    })
-    cy.window().then((win) => {
-      const variant = 'variant-03'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting - ${variant}`)
-    })
-    cy.window().then((win) => {
-      const variant = 'variant-04'
-      win.postMessage(variant)
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting - ${variant}`)
-    })
+    cy.get('#variant-01').click()
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-variant-01`)
+    cy.get('canvas').percySnapshot(`transparent-geoms-sorting - variant-01`)
+
+    cy.get('#variant-02').click()
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-variant-02`)
+    cy.get('canvas').percySnapshot(`transparent-geoms-sorting - variant-02`)
+
+    cy.get('#variant-03').click()
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-variant-03`)
+    cy.get('canvas').percySnapshot(`transparent-geoms-sorting - variant-02`)
+
+    cy.get('#variant-04').click()
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-variant-04`)
+    cy.get('canvas').percySnapshot(`transparent-geoms-sorting - variant-02`)
   })
 })
