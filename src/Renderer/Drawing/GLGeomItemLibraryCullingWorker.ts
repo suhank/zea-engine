0717f910d394acfa6931d4523515851ffa6f9e69
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-28 15:16:05 -0700

    refactor: migrated GLGeomItemLibraryCullingWorker

--- src/Renderer/Drawing/GLGeomItemLibraryCullingWorker.ts
+++ src/Renderer/Drawing/GLGeomItemLibraryCullingWorker.ts
@@ -1,35 +1,38 @@
+import { Quat, Vec2, Vec3 } from '../../Math'
+import { GeomItem } from '../../SceneTree'
+
 /* eslint-disable camelcase */
-const vec3_normalize = (vec) => {
+const vec3_normalize = (vec: Vec3): Vec3 => {
   let len = vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]
   if (len < Number.EPSILON) {
-    return [0, 0, 0]
+    return new Vec3(0, 0, 0)
   }
   len = 1.0 / Math.sqrt(len)
-  return [vec[0] * len, vec[1] * len, vec[2] * len]
+  return new Vec3(vec[0] * len, vec[1] * len, vec[2] * len)
 }
-const vec3_subtract = (vec1, vec2) => {
-  return [vec1[0] - vec2[0], vec1[1] - vec2[1], vec1[2] - vec2[2]]
+const vec3_subtract = (vec1: Vec3, vec2: Vec3): Vec3 => {
+  return new Vec3(vec1[0] - vec2[0], vec1[1] - vec2[1], vec1[2] - vec2[2])
 }
-const vec3_dot = (vec1, vec2) => {
+const vec3_dot = (vec1: Vec3, vec2: Vec3) => {
   return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]
 }
-const vec3_length = (vec) => {
+const vec3_length = (vec: Vec3): number => {
   return Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2])
 }
-const vec3_scale = (vec, scl) => {
-  return [vec[0] * scl, vec[1] * scl, vec[2] * scl]
+const vec3_scale = (vec: Vec3, scl: number): Vec3 => {
+  return new Vec3(vec[0] * scl, vec[1] * scl, vec[2] * scl)
 }
-const vec2_scale = (vec, scl) => {
-  return [vec[0] * scl, vec[1] * scl]
+const vec2_scale = (vec: Vec2, scl: number): Vec2 => {
+  return new Vec2(vec[0] * scl, vec[1] * scl)
 }
-const vec2_length = (vec) => {
+const vec2_length = (vec: Vec2): number => {
   return Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
 }
 
-const quat_conjugate = (quat) => {
-  return [-quat[0], -quat[1], -quat[2], quat[3]]
+const quat_conjugate = (quat: Quat): Quat => {
+  return new Quat(-quat[0], -quat[1], -quat[2], quat[3])
 }
-const quat_multiply = (quat1, quat2) => {
+const quat_multiply = (quat1: Quat, quat2: Quat): Quat => {
   const ax = quat1[0]
   const ay = quat1[1]
   const az = quat1[2]
@@ -39,29 +42,29 @@ const quat_multiply = (quat1, quat2) => {
   const bz = quat2[2]
   const bw = quat2[3]
 
-  return [
+  return new Quat(
     ax * bw + aw * bx + ay * bz - az * by,
     ay * bw + aw * by + az * bx - ax * bz,
     az * bw + aw * bz + ax * by - ay * bx,
-    aw * bw - ax * bx - ay * by - az * bz,
-  ]
+    aw * bw - ax * bx - ay * by - az * bz
+  )
 }
-const quat_rotateVec3 = (quat, vec3) => {
-  const vq = [vec3[0], vec3[1], vec3[2], 0.0]
+const quat_rotateVec3 = (quat: Quat, vec3: Vec3): Vec3 => {
+  const vq = new Quat(vec3[0], vec3[1], vec3[2], 0.0)
   const pq = quat_multiply(quat_multiply(quat, vq), quat_conjugate(quat))
-  return [pq[0], pq[1], pq[2]]
+  return new Vec3(pq[0], pq[1], pq[2])
 }
 
 // ///////////////////////////////////////////////
 // View data.
-const geomItemsData = []
-const frustumCulled = []
+const geomItemsData: any[] = []
+const frustumCulled: any[] = []
 let culledCount = 0
-let newlyCulled = []
-let newlyUnCulled = []
+let newlyCulled: any[] = []
+let newlyUnCulled: any[] = []
 
-let cameraPos
-let cameraInvOri
+let cameraPos: Vec3
+let cameraInvOri: any
 let isOrthographic = false
 let frustumHeight = 0
 let frustumWidth = 0
@@ -69,14 +72,14 @@ let frustumHalfAngleX = 0
 let frustumHalfAngleY = 0
 let solidAngleLimit = 0.004
 
-const cull = (index) => {
+const cull = (index: number) => {
   if (!frustumCulled[index]) {
     frustumCulled[index] = true
     culledCount++
     newlyCulled.push(index)
   }
 }
-const unCull = (index) => {
+const unCull = (index: number) => {
   if (frustumCulled[index]) {
     frustumCulled[index] = false
     culledCount--
@@ -84,7 +87,7 @@ const unCull = (index) => {
   }
 }
 
-const checkGeomItem = (geomItemData) => {
+const checkGeomItem = (geomItemData: any) => {
   if (!geomItemData || !cameraPos) return
 
   // Some items, like Handles that
@@ -110,7 +113,7 @@ const checkGeomItem = (geomItemData) => {
     // further away, so the solid angle calculated above gets smaller.
     // This was causing items with big bounding spheres to be culled too early
     // at the corner of the screen.
-    const vec = vec3_subtract(geomItemData.pos, cameraPos)
+    const vec: Vec3 = vec3_subtract(geomItemData.pos, cameraPos)
     const viewPos = quat_rotateVec3(cameraInvOri, vec)
     if (
       Math.abs(viewPos[0]) - boundingRadius > frustumWidth * 0.5 ||
@@ -144,8 +147,8 @@ const checkGeomItem = (geomItemData) => {
     // This was causing items with big bounding spheres to be culled too early
     // at the corner of the screen.
     const viewPos = quat_rotateVec3(cameraInvOri, vec)
-    const viewVecXZ = [viewPos[0], viewPos[2]]
-    const viewVecYZ = [viewPos[1], viewPos[2]]
+    const viewVecXZ = new Vec2(viewPos[0], viewPos[2])
+    const viewVecYZ = new Vec2(viewPos[1], viewPos[2])
     const distX = vec2_length(viewVecXZ)
     const distY = vec2_length(viewVecYZ)
     const solidAngleXZ = Math.asin(boundingRadius / distX)
@@ -176,7 +179,7 @@ const checkGeomItem = (geomItemData) => {
   unCull(geomItemData.id)
 }
 
-const onViewPortChanged = (data, postMessage) => {
+const onViewPortChanged = (data: any, postMessage: any) => {
   if (data.isOrthographic) {
     isOrthographic = true
     frustumHeight = data.frustumHeight
@@ -194,7 +197,7 @@ const onViewPortChanged = (data, postMessage) => {
   }
 }
 
-const onViewChanged = (data, postMessage) => {
+const onViewChanged = (data: any, postMessage: any) => {
   cameraPos = data.cameraPos
   cameraInvOri = quat_conjugate(data.cameraOri)
   solidAngleLimit = data.solidAngleLimit
@@ -202,7 +205,7 @@ const onViewChanged = (data, postMessage) => {
   onDone(postMessage)
 }
 
-const onDone = (postMessage) => {
+const onDone = (postMessage: any) => {
   // console.log('onDone newlyCulled:', newlyCulled.length, 'newlyUnCulled:', newlyUnCulled.length)
   if (newlyCulled.length > 0 || newlyUnCulled.length > 0) {
     // console.log('CullResults culled:', culledCount, 'visible:', geomItemsData.length - 1 - culledCount)
@@ -214,16 +217,16 @@ const onDone = (postMessage) => {
   }
 }
 
-const handleMessage = (data, postMessage) => {
+const handleMessage = (data: any, postMessage: any) => {
   if (data.type == 'ViewportChanged') {
     onViewPortChanged(data, postMessage)
   } else if (data.type == 'ViewChanged') {
     onViewChanged(data, postMessage)
   } else if (data.type == 'UpdateGeomItems') {
-    data.removedItemIndices.forEach((id) => {
+    data.removedItemIndices.forEach((id: number) => {
       geomItemsData[id] = null
     })
-    data.geomItems.forEach((geomItem) => {
+    data.geomItems.forEach((geomItem: any) => {
       // New geoms default to being un-culled
       if (!geomItemsData[geomItem.id]) frustumCulled[geomItem.id] = false
       geomItemsData[geomItem.id] = geomItem
