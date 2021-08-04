110fe1462169bb17106056be69c696f3d993f6ac
Seiji-zea Smith <seiji.smith@zea.live>
2021-08-04 14:36:56 -0700

    refactor: surfaceHighlight.glsl now manages relevant includes/functions

--- src/Renderer/Shaders/GLSL/surfaceHighlight.glsl
+++ src/Renderer/Shaders/GLSL/surfaceHighlight.glsl
@@ -1,4 +1,20 @@
-int drawItemId = int(v_drawItemId + 0.5);
-fragColor =  getHighlightColor(drawItemId);
+// int drawItemId = int(v_drawItemId + 0.5);
+// fragColor =  getHighlightColor(drawItemId);
 
 
+  import 'GLSLUtils.glsl'
+  import 'drawItemTexture.glsl'
+
+  #ifdef ENABLE_FLOAT_TEXTURES
+  vec4 getHighlightColor(int id) {
+    return fetchTexel(instancesTexture, instancesTextureSize, (id * pixelsPerItem) + 4);
+  }
+  #else
+
+  uniform vec4 highlightColor;
+
+  vec4 getHighlightColor(int id) {
+      return highlightColor;
+  }
+
+  #endif
