f6373f1e76a17873d864dfdd0e9da719e28a296e
Seiji-zea Smith <seiji.smith@zea.live>
2021-08-05 09:57:17 -0700

    refactor: migrated GLLinesItemSet

--- src/Renderer/Drawing/GLLinesItemSet.ts
+++ src/Renderer/Drawing/GLLinesItemSet.ts
@@ -8,12 +8,12 @@ import '../../SceneTree/Geometry/Mesh'
 class GLLinesItemSet extends GLGeomItemSetMultiDraw {
   /**
    * Draw an item to screen.
-   * @param {object} renderstate - The object tracking the current state of the renderer
-   * @param {number} count - the element count for this draw call.
-   * @param {number} offset - the element offset for this draw call.
+   * @param {Record<any,any>} renderstate - The object tracking the current state of the renderer
+   * @param {Int32Array} count - the element count for this draw call.
+   * @param {Int32Array} offset - the element offset for this draw call.
    */
-  singleDraw(renderstate, count, offset) {
-    const gl = this.gl
+  singleDraw(renderstate: Record<any, any>, count: Int32Array, offset: Int32Array) {
+    const gl = <Record<any, any>>this.gl
     const { occluded } = renderstate.unifs
     if (occluded) {
       gl.uniform1i(occluded.location, 0)
@@ -24,20 +24,20 @@ class GLLinesItemSet extends GLGeomItemSetMultiDraw {
     if (occluded) {
       gl.uniform1i(occluded.location, 1)
       gl.depthFunc(gl.GREATER)
-      gl.multiDrawElements(gl.LINES, counts, 0, gl.UNSIGNED_INT, offsets, 0, counts.length)
+      gl.multiDrawElements(gl.LINES, count, 0, gl.UNSIGNED_INT, offset, 0, count.length)
       gl.depthFunc(gl.LEQUAL)
     }
   }
 
   /**
    * Draw an item to screen.
-   * @param {object} renderstate - The object tracking the current state of the renderer
+   * @param {Record<any,any>} renderstate - The object tracking the current state of the renderer
    * @param {Int32Array} drawIds - the draw id for each element drawn in by this draw call.
    * @param {Int32Array} counts - the geom element count for each element drawn in by this draw call.
    * @param {Int32Array} offsets - the geom element offset for each element drawn in by this draw call.
    */
-  multiDraw(renderstate, drawIds, counts, offsets) {
-    const gl = this.gl
+  multiDraw(renderstate: Record<any, any>, drawIds: Int32Array, counts: Int32Array, offsets: Int32Array) {
+    const gl = <Record<any, any>>this.gl
     if (gl.multiDrawArrays) {
       const { occluded } = renderstate.unifs
       if (occluded) {
