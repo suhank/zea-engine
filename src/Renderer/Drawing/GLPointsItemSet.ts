f80fc848d14fc06f55591bc5117ea4d8d88a131e
Seiji-zea Smith <seiji.smith@zea.live>
2021-08-05 10:16:37 -0700

    refactor: migrated GLPointsItemSet

--- src/Renderer/Drawing/GLPointsItemSet.ts
+++ src/Renderer/Drawing/GLPointsItemSet.ts
@@ -8,24 +8,24 @@ import '../../SceneTree/Geometry/Mesh'
 class GLPointsItemSet extends GLGeomItemSetMultiDraw {
   /**
    * Draw an item to screen.
-   * @param {object} renderstate - The object tracking the current state of the renderer
+   * @param {Record<any,any>} renderstate - The object tracking the current state of the renderer
    * @param {number} count - the element count for this draw call.
    * @param {number} offset - the element offset for this draw call.
    */
-  singleDraw(renderstate, count, offset) {
+  singleDraw(renderstate: Record<any, any>, count: number, offset: number) {
     const gl = this.gl
     gl.drawArrays(gl.POINTS, offset, count)
   }
 
   /**
    * Draw an item to screen.
-   * @param {object} renderstate - The object tracking the current state of the renderer
+   * @param {Record<any,any>} renderstate - The object tracking the current state of the renderer
    * @param {Int32Array} drawIds - the draw id for each element drawn in by this draw call.
    * @param {Int32Array} counts - the geom element count for each element drawn in by this draw call.
    * @param {Int32Array} offsets - the geom element offset for each element drawn in by this draw call.
    */
-  multiDraw(renderstate, drawIds, counts, offsets) {
-    const gl = this.gl
+  multiDraw(renderstate: Record<any, any>, drawIds: Int32Array, counts: Int32Array, offsets: Int32Array) {
+    const gl = <Record<any, any>>this.gl
     if (gl.multiDrawArrays) {
       gl.multiDrawArrays(gl.POINTS, offsets, 0, counts, 0, counts.length)
     } else {
