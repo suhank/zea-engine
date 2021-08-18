c2386bf7b26b7597145e8c295e6ee45e23028bf6
Seiji-zea Smith <seiji.smith@zea.live>
2021-08-18 15:27:28 -0700

    refactor: multichoice parameter refactor

--- src/SceneTree/Parameters/MultiChoiceParameter.ts
+++ src/SceneTree/Parameters/MultiChoiceParameter.ts
@@ -27,7 +27,7 @@ class MultiChoiceParameter extends NumberParameter {
    * @param {number} index - The index value.
    * @param {array} choices - The choices value.
    */
-  constructor(name?: string, index?: number, choices?: any[]) {
+  constructor(name?: string, index?: number, choices: any[] = []) {
     super(name, index, [0, choices.length], 1)
     this.choices = choices
   }
