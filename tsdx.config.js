4f8ec7ad4226cd8f332887151ef6afae4a770d2d
Philip Taylor <phil@zea.live>
2021-10-15 17:16:57 -0400

    build: migrating to tsc + rollup build process.

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,19 +1,25 @@
 const glslify = require('rollup-plugin-glslify')
 const { base64 } = require('rollup-plugin-base64')
 const webWorkerLoader = require('rollup-plugin-web-worker-loader')
-
-const glslOptions = {
-  compress: false
-}
+// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
+const replace = require('@rollup/plugin-replace')
 
 module.exports = {
-  rollup(config) {
+  rollup(config, opts) {
+    config.plugins = config.plugins.map(p =>
+      p.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(opts.env),
+            preventAssignment: true
+          })
+        : p
+    )
     config.plugins = [
-      glslify(glslOptions),
+      glslify({ compress: false }),
       base64({ include: '**/*.wasm' }),
-      webWorkerLoader({ 
-        extensions: ['.ts', '.js'], 
-        pattern: /.+\-worker\.(?:js|ts)$/ 
+      webWorkerLoader({
+        extensions: ['.ts', '.js'],
+        pattern: /.+\-worker\.(?:js|ts)$/
       }),
       ...config.plugins
     ]
