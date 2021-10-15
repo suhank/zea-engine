4c8d5e12c49f08fbec436ff656d011f9899543ba
Philip Taylor <phil@zea.live>
2021-10-15 15:12:52 -0400

    test: adding explicit event to say the label is highlighted.

--- cypress/integration/labels-spec.js
+++ cypress/integration/labels-spec.js
@@ -9,6 +9,7 @@ describe('Labels and Billboards', () => {
     cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-Loading')
 
     cy.get('canvas').trigger('mousemove', 500, 330)
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-MouseOverLabel`)
     cy.get('canvas').percySnapshot(`MouseOverLabel`)
 
     cy.window().then((win) => {
