ede72719ae4970fe6f047e862c2f1c1ce648e2e4
Philip Taylor <phil@zea.live>
2021-10-20 19:16:10 -0400

    refactor: cleaned up regressions in the Procedural Sphere class.

--- src/SceneTree/Geometry/Shapes/Sphere.ts
+++ src/SceneTree/Geometry/Shapes/Sphere.ts
@@ -21,19 +21,19 @@ import { Vec2Attribute } from '../Vec2Attribute'
  */
 class Sphere extends ProceduralMesh {
   /**
-   * @member {NumberParameter} loopsParam - Specifies the number of subdivisions(stacks) along the `Z` axis.
-   */
-  loopsParam: NumberParameter
-
-  /**
    * @member {NumberParameter} radiusParam - Radius of the sphere.
    */
-  radiusParam: NumberParameter
+  radiusParam: NumberParameter = new NumberParameter('Radius', 1.0)
 
   /**
    * @member {NumberParameter} sidesParam - Specifies the number of subdivisions around the `Z` axis.
    */
-  sidesParam: NumberParameter
+  sidesParam: NumberParameter = new NumberParameter('Sides', 12, [3, 200], 1)
+
+  /**
+   * @member {NumberParameter} loopsParam - Specifies the number of subdivisions(stacks) along the `Z` axis.
+   */
+  loopsParam: NumberParameter = new NumberParameter('Loops', 6, [3, 200], 1)
 
   /**
    * Creates an instance of Sphere.
@@ -48,13 +48,13 @@ class Sphere extends ProceduralMesh {
 
     if (isNaN(radius) || isNaN(sides) || isNaN(loops)) throw new Error('Invalid geom args')
 
-    this.radiusParam = this.addParameter(new NumberParameter('Radius', radius)) as NumberParameter
-    this.sidesParam = this.addParameter(
-      new NumberParameter('Sides', sides >= 3 ? sides : 3, [3, 200], 1)
-    ) as NumberParameter
-    this.loopsParam = this.addParameter(
-      new NumberParameter('Loops', loops >= 3 ? loops : 3, [3, 200], 1)
-    ) as NumberParameter
+    this.addParameter(this.radiusParam)
+    this.addParameter(this.sidesParam)
+    this.addParameter(this.loopsParam)
+
+    this.radiusParam.value = radius
+    this.sidesParam.value = sides
+    this.loopsParam.value = loops
 
     this.addVertexAttribute('texCoords', new Vec2Attribute())
     this.addVertexAttribute('normals', new Vec3Attribute())
@@ -68,9 +68,9 @@ class Sphere extends ProceduralMesh {
    * @private
    */
   rebuild(): void {
-    const radius = this.radiusParam.value || 1.0
-    const nbSides = this.sidesParam.value || 12
-    const nbLoops = this.loopsParam.value || 12
+    const radius = this.radiusParam.value
+    const nbSides = this.sidesParam.value
+    const nbLoops = this.loopsParam.value
 
     const numVertices = 2 + nbSides * nbLoops
     const numTris = nbSides * 2
@@ -85,10 +85,10 @@ class Sphere extends ProceduralMesh {
     const normals = <Vec3Attribute>this.getVertexAttribute('normals')
     const normal = new Vec3(0.0, 0.0, 1.0)
     let vertex = 0
-    if (!positions || !normals) return
+    if (!positions) return
 
     positions.getValueRef(vertex).set(0.0, 0.0, radius)
-    normals.getValueRef(vertex).set(0.0, 0.0, 1.0)
+    if (normals) normals.getValueRef(vertex).set(0.0, 0.0, 1.0)
     vertex++
 
     for (let i = 0; i < nbLoops; i++) {
@@ -105,64 +105,66 @@ class Sphere extends ProceduralMesh {
     }
 
     positions.getValueRef(vertex).set(0.0, 0.0, -radius)
-    normals.getValueRef(vertex).set(0.0, 0.0, -1.0)
+    if (normals) normals.getValueRef(vertex).set(0.0, 0.0, -1.0)
     vertex++
 
     // ////////////////////////////
     // Build the topology
     const texCoords = <Vec2Attribute>this.getVertexAttribute('texCoords')
-    if (texCoords) {
-      // build the fan at the first pole.
-      let faceIndex = 0
-      for (let j = 0; j < nbSides; j++) {
-        const v0 = 0
-        const v1 = ((j + 1) % nbSides) + 1
-        const v2 = j + 1
-        this.setFaceVertexIndices(faceIndex, [v0, v1, v2])
 
-        if (texCoords) {
-          const uv0 = new Vec2(0.5, 0.0)
-          const uv1 = new Vec2(1.0 - (j + 1) / nbSides, 0.0)
-          const uv2 = new Vec2(1.0 - j / nbSides, 1.0 / (nbLoops + 1))
-          texCoords.setFaceVertexValue(faceIndex, 0, uv0)
-          texCoords.setFaceVertexValue(faceIndex, 1, uv1)
-          texCoords.setFaceVertexValue(faceIndex, 2, uv2)
-        }
+    // build the fan at the first pole.
+    let faceIndex = 0
+    for (let j = 0; j < nbSides; j++) {
+      const v0 = 0
+      const v1 = ((j + 1) % nbSides) + 1
+      const v2 = j + 1
+      this.setFaceVertexIndices(faceIndex, [v0, v1, v2])
 
-        faceIndex++
-      }
-
-      // Build the fan at the second pole.
-      for (let j = 0; j < nbSides; j++) {
-        const v0 = numVertices - 1
-        const v1 = nbSides * (nbLoops - 1) + j + 1
-        const v2 = nbSides * (nbLoops - 1) + ((j + 1) % nbSides) + 1
-        this.setFaceVertexIndices(faceIndex, [v0, v1, v2])
-
-        const uv0 = new Vec2(1.0 - j / nbSides, nbLoops / (nbLoops + 1))
-        const uv1 = new Vec2(1.0 - (j + 1) / nbSides, nbLoops / (nbLoops + 1))
-        const uv2 = new Vec2(0.5, 1.0)
+      if (texCoords) {
+        const uv0 = new Vec2(0.5, 0.0)
+        const uv1 = new Vec2((j + 1) / (nbSides - 1), 1 / (nbLoops + 1))
+        const uv2 = new Vec2(j / (nbSides - 1), 1 / (nbLoops + 1))
         texCoords.setFaceVertexValue(faceIndex, 0, uv0)
         texCoords.setFaceVertexValue(faceIndex, 1, uv1)
         texCoords.setFaceVertexValue(faceIndex, 2, uv2)
-
-        faceIndex++
       }
 
-      for (let i = 0; i < nbLoops - 1; i++) {
-        for (let j = 0; j < nbSides; j++) {
-          const v0 = nbSides * i + j + 1
-          const v1 = nbSides * i + ((j + 1) % nbSides) + 1
-          const v2 = nbSides * (i + 1) + ((j + 1) % nbSides) + 1
-          const v3 = nbSides * (i + 1) + j + 1
-          this.setFaceVertexIndices(faceIndex, [v0, v1, v2, v3])
+      faceIndex++
+    }
+    // Build the fan at the second pole.
+    for (let j = 0; j < nbSides; j++) {
+      const v0 = numVertices - 1
+      const v2 = nbSides * (nbLoops - 1) + ((j + 1) % nbSides) + 1
+      const v1 = nbSides * (nbLoops - 1) + j + 1
+      this.setFaceVertexIndices(faceIndex, [v0, v1, v2])
 
-          texCoords.setFaceVertexValue(faceIndex, 0, new Vec2(i / nbLoops, j / nbLoops))
-          texCoords.setFaceVertexValue(faceIndex, 1, new Vec2(i / nbLoops, (j + 1) / nbLoops))
-          texCoords.setFaceVertexValue(faceIndex, 2, new Vec2((i + 1) / nbLoops, (j + 1) / nbLoops))
-          texCoords.setFaceVertexValue(faceIndex, 3, new Vec2((i + 1) / nbLoops, j / nbLoops))
-          faceIndex++
+      if (texCoords) {
+        const uv0 = new Vec2(0.5, 1.0)
+        const uv1 = new Vec2((j + 1) / (nbSides - 1), 1 - 1 / (nbLoops + 1))
+        const uv2 = new Vec2(j / (nbSides - 1), 1 - 1 / (nbLoops + 1))
+        texCoords.setFaceVertexValue(faceIndex, 0, uv0)
+        texCoords.setFaceVertexValue(faceIndex, 1, uv1)
+        texCoords.setFaceVertexValue(faceIndex, 2, uv2)
+      }
+
+      faceIndex++
+    }
+
+    for (let i = 0; i < nbLoops - 1; i++) {
+      for (let j = 0; j < nbSides; j++) {
+        const v0 = nbSides * i + j + 1
+        const v1 = nbSides * i + ((j + 1) % nbSides) + 1
+        const v2 = nbSides * (i + 1) + ((j + 1) % nbSides) + 1
+        const v3 = nbSides * (i + 1) + j + 1
+        this.setFaceVertexIndices(faceIndex, [v0, v1, v2, v3])
+
+        if (texCoords) {
+          texCoords.setFaceVertexValue(faceIndex, 0, new Vec2(j / nbSides, (i + 1) / nbLoops))
+          texCoords.setFaceVertexValue(faceIndex, 1, new Vec2((j + 1) / nbSides, (i + 1) / nbLoops))
+          texCoords.setFaceVertexValue(faceIndex, 2, new Vec2((j + 1) / nbSides, (i + 2) / nbLoops))
+          texCoords.setFaceVertexValue(faceIndex, 3, new Vec2(j / nbSides, (i + 2) / nbLoops))
         }
+        faceIndex++
       }
     }
   }
