d85a3d715ad8bc77e6d6180286fbaf6746fd75ac
Philip Taylor <phil@zea.live>
2021-10-22 10:40:20 -0400

    fix: The Plane preimitive faces were facking backwards which is  now fixed.

--- src/SceneTree/Geometry/Shapes/Plane.ts
+++ src/SceneTree/Geometry/Shapes/Plane.ts
@@ -80,9 +80,9 @@ class Plane extends ProceduralMesh {
     for (let i = 0; i < detailY; i++) {
       for (let j = 0; j < detailX; j++) {
         const v0 = (detailX + 1) * (i + 1) + j
-        const v1 = (detailX + 1) * (i + 1) + (j + 1)
+        const v1 = (detailX + 1) * i + j
         const v2 = (detailX + 1) * i + (j + 1)
-        const v3 = (detailX + 1) * i + j
+        const v3 = (detailX + 1) * (i + 1) + (j + 1)
         this.setFaceVertexIndices(quadId, [v0, v1, v2, v3])
         quadId = quadId + 1
       }
