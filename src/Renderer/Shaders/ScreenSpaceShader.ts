231558317d9ca2dd30c3afb244a758035acfe7b5
Philip Taylor <phil@zea.live>
2021-11-10 17:14:16 -0500

    WIP: ScreenSpaceShader now implements getMaterialTemplate():

--- src/Renderer/Shaders/ScreenSpaceShader.ts
+++ src/Renderer/Shaders/ScreenSpaceShader.ts
@@ -42,6 +42,16 @@ class ScreenSpaceShader extends GLShader {
     matData[3] = baseColor.a
     return matData
   }
+
+  /**
+   * Each shader provides a template material that each material instance is
+   * based on. The shader specifies the parameters needed by the shader, and
+   * the material provides values to the shader during rendering.
+   * @return - The template material value.
+   */
+  static getMaterialTemplate(): Material {
+    return material
+  }
 }
 
 const material = new Material('ScreenSpaceShader_template')
