b2883b73df7f85793f6e30fe7614fff088d9eee7
Philip Taylor <phil@zea.live>
2022-01-06 08:45:11 -0500

    test: cleaned up occlusion culling tests.

---
+++ cypress/integration/occlusion-culling-occluded-by-hidden-bbox-spec.js
@@ -0,0 +1,26 @@
+describe('occlusion-culling-occluded-by-hidden-bbox', () => {
+  it('Capture snapshots', () => {
+    cy.visit('testing-e2e/occlusion-culling-occluded-by-hidden-bbox.html', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage')
+      },
+    })
+
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
+    cy.get('canvas').percySnapshot('occlusion-culling-occluded-by-hidden-bbox')
+
+    cy.window().then((win) => {
+      const variant = 'variant-01'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`occlusion-culling-occluded-by-hidden-bbox - ${variant}`)
+    })
+
+    cy.window().then((win) => {
+      const variant = 'variant-02'
+      win.postMessage(variant)
+      cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
+      cy.get('canvas').percySnapshot(`occlusion-culling-occluded-by-hidden-bbox - ${variant}`)
+    })
+  })
+})
--- cypress/integration/occlusion-culling-tiny-objects-spec.js
+++ cypress/integration/occlusion-culling-tiny-objects-spec.js
@@ -1,26 +1,26 @@
-// describe('occlusion-culling2', () => {
+// describe('occlusion-culling', () => {
 //   it('Capture snapshots', () => {
-//     cy.visit('testing-e2e/occlusion-culling2.html', {
+//     cy.visit('testing-e2e/occlusion-culling.html', {
 //       onBeforeLoad(win) {
 //         cy.spy(win, 'postMessage').as('postMessage')
 //       },
 //     })
 
 //     cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-loading')
-//     cy.get('canvas').percySnapshot('occlusion-culling2')
+//     cy.get('canvas').percySnapshot('occlusion-culling')
 
 //     cy.window().then((win) => {
 //       const variant = 'variant-01'
 //       win.postMessage(variant)
 //       cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-//       cy.get('canvas').percySnapshot(`occlusion-culling2 - ${variant}`)
+//       cy.get('canvas').percySnapshot(`occlusion-culling - ${variant}`)
 //     })
 
 //     cy.window().then((win) => {
 //       const variant = 'variant-02'
 //       win.postMessage(variant)
 //       cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-//       cy.get('canvas').percySnapshot(`occlusion-culling2 - ${variant}`)
+//       cy.get('canvas').percySnapshot(`occlusion-culling - ${variant}`)
 //     })
 //   })
 // })
